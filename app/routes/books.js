3637c2094c1212776a0ccae9612075bef0504f6d
Adam Skoczylas <skocadam@gmail.com>
2018-02-07 22:55:07 +0100

    need to fix up books model and route

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,23 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { hash } from 'rsvp';
 
 export default Route.extend({
+  model() {
+    return hash({
+      books: this.store.findAll("book"),
+      authors: this.store.findAll("author"),
+      libraries: this.store.findAll("library")
+    });
+  },
+
+  setupController(controller, model) {
+    const books = model.books;
+    const authors = model.authors;
+    const libraries = model.libraries;
+
+    this._super(controller, books);
+
+    controller.set("authors", authors);
+    controller.set("libraries", libraries);
+  }
 });
